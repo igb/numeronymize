a0b9851916a57860edad5c10de7fdaf9dbd7f302
Ian Brown <ibrown@netflix.com>
2024-07-04 22:36:08 -0700

    seems to work

--- numeronymize.el
+++ numeronymize.el
@@ -1,5 +1,5 @@
 (defun numeronymize()
-  "Abbreviate a word using a numerical contraction or numeronym (e.g. Abbreviating \"enshittification\" as \"e14n\"."
+  "Abbreviate a word using a numerical contraction or numeronym (e.g. Abbreviating \"enshittification\" as \"e14n\")."
   
   (interactive)
   
@@ -9,26 +9,21 @@
 	    (selection (buffer-substring-no-properties (region-beginning) (region-end))))
 	(if (= (length selection) 0)
 	    (print "nothing selected")
-
-	  (setq replacement (read-string "'replace' with: "))
-
-	  (if (= (length replacement) 0)
-	      (print "no replacement text?")
-	    (setq slength  (length selection))
-	    (setq count 0)
-            (setq acc "")
-	    
-	    
-	    (while (< count slength)
-	      (setq acc (concat acc "^H"))
-	      (setq count (1+ count)))
-	    (kill-region (region-beginning) (region-end))
-	    (insert (concat selection acc " " replacement))
-	    
+	  (if (string-match "[[:space:]]" selection)
+	      (print "selection contains whitespace")
+	    (if (< (length selection) 3)
+		(print "selection must be longer than 3 characters")
+	      (setq numsize  (- (length selection) 2))
+	      (setq start (substring selection 0 1))
+	      (setq end  (substring selection (- (length selection) 1) (length selection)))
+	      (kill-region (region-beginning) (region-end))
+	      (insert (concat start (number-to-string numsize) end))
+	      
+	      )
 	    )
 	  )
 	)
-	(print "nothing selected")
-     
+    (print "nothing selected")
+    
     )
   )
